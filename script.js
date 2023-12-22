e84d2bacc6b48887759c10c4a06735fa40a9adfa
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2023-12-22 20:39:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('mobile-menu').addEventListener('click', function () {
+  document.querySelector('.nav').classList.toggle('active');
+  const submenu = document.querySelector('.submenu');
+
+  nav.classList.toggle('active');
+  submenu.style.display = 'none'; // Hide sub-menu when toggling the main menu
+
+  // Toggle the display of sub-menu
+  document.querySelector('.has-submenu').addEventListener('click', function (event) {
+    event.preventDefault();
+    submenu.style.display = (submenu.style.display === 'block' ? 'none' : 'block');
+  });
+});
